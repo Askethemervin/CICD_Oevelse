3ea3e4b09b308736c5ab1c16fe8e0e1fc5eddb2c
Askethemervin <Aske.johanson@gmail.com>
2024-09-19 13:18:54 +0200

    commit

---
+++ src/main/java/org/example/cicdoevelse/CicdOevelseApplication.java
@@ -0,0 +1,13 @@
+package org.example.cicdoevelse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdOevelseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdOevelseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cicdoevelse/CicdOevelseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cicdoevelse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdOevelseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
